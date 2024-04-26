b97f42f55bfd967d9046836f9bff96356e25c731
Julian Sarli <sarlij485@gmail.com>
2024-04-25 23:31:55 -0400

    testitytest

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-document.getElementById('requestForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+// document.getElementById('requestForm').addEventListener('submit', function(event) {
+//     event.preventDefault();
     
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var subject = document.getElementById('subject').value;
-    var message = document.getElementById('message').value;
+//     var name = document.getElementById('name').value;
+//     var email = document.getElementById('email').value;
+//     var subject = document.getElementById('subject').value;
+//     var message = document.getElementById('message').value;
 
-    var selectedLanguage = document.querySelector('input[name="language"]:checked').value;
+//     var selectedLanguage = document.querySelector('input[name="language"]:checked').value;
 
-    var mailtoLink = "mailto:jostlearn@gmail.com" +
-                      `?cc=${email}` +
-                      `&subject=From: ${encodeURIComponent(name)} | ${encodeURIComponent(subject)}` +
-                      ` (${encodeURIComponent(selectedLanguage)})` +
-                      `&body=${encodeURIComponent(message)}`;
+//     var mailtoLink = "mailto:jostlearn@gmail.com" +
+//                       `?cc=${email}` +
+//                       `&subject=From: ${encodeURIComponent(name)} | ${encodeURIComponent(subject)}` +
+//                       ` (${encodeURIComponent(selectedLanguage)})` +
+//                       `&body=${encodeURIComponent(message)}`;
     
-    window.open(mailtoLink, '_blank');
-});
+//     window.open(mailtoLink, '_blank');
+// });
 
 var textarea = document.getElementById('message');
 textarea.addEventListener('input', function() {
